bb24157e9c42b266e0a68a964157e3ed2bb6311a
4TechGuns <fourtechgunstest@outlook.com>
2022-05-29 19:42:02 -0230

    write unit tests

---
+++ ThirtyDollarLib.Tests/SequenceTests.cs
@@ -0,0 +1,29 @@
+﻿namespace ThirtyDollarLib.Tests
+{
+    [TestClass]
+    public class SequenceTests
+    {
+        [TestMethod]
+        public void BasicGeneration()
+        {
+            List<Item> items = new() { new Item(ItemType.Boom), new Item(ItemType.Clap) };
+            Sequence seq = new(items);
+
+            string result = seq.ToString();
+            Assert.AreEqual("boom@0|👏@0", result);
+        }
+
+        [TestMethod]
+        public void Reserialisation()
+        {
+            List<Item> items = new() { new Item(ItemType.Boom), new Item(ItemType.Clap) };
+            Sequence seq = new(items);
+
+            string result = seq.ToString();
+            Sequence seqP = Sequence.Parse(result);
+
+            string result2 = seq.ToString();
+            Assert.AreEqual("boom@0|👏@0", result2);
+        }
+    }
+}
